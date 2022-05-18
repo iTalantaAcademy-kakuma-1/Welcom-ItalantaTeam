ea3a577c60980d88091cfe33720aba49a5b6d9bd
Kambale Kibeho <dessaikibeho@gmail.com>
2022-05-18 12:56:55 +0300

    desssai

--- src/Welcom-All-Pages/HomePage/Home.js
+++ src/Welcom-All-Pages/HomePage/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "./Home.css"
 import LogoItalanta from "./image/My-project 5.jpg"
+import{Link} from "react-router-dom"
 
 function Home() {
   return (
     <div className='Title'>
       <img src={LogoItalanta}  alt="loading..." />
       <h1>Thank You!</h1>
-      <a href='/dessaikibeho' style={{color:'blue', fontSize:'70px'}} className='Next'>-</a>
+      <Link to='/dessaikibeho' style={{color:'blue', fontSize:'70px'}} className='Next'>-</Link>
 
 
     </div>
