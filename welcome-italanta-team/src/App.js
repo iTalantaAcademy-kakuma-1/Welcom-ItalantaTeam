b87431f1fef2868c83869e466e1cb4657a03c7e6
Aljabana Elizabeth Mubarak <93643180+aljabana002@users.noreply.github.com>
2022-05-24 12:38:52 +0300

    alja

--- welcome-italanta-team/src/App.js
+++ welcome-italanta-team/src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
+import "./Welcom-All-Pages/Aljabana-Elizabeth/Aljabana.css"
 import"./Welcom-All-Pages/Welcom.css"
 import DessaiKibeho from "./Welcom-All-Pages/DessaiKibeho/DessaiKibeho"
 import WelcomePage from './Welcom-All-Pages/WelcomePage';
 import AllPagesInOne from './Welcom-All-Pages/All-Pages-In-One';
+import Aljabana  from './Welcom-All-Pages/Aljabana-Elizabeth/Aljabana';
+
 
 
 
@@ -15,7 +18,7 @@ function App() {
     <body>
       <WelcomePage/>
       <AllPagesInOne/>
-
+    <Aljabana/>
     </body>
   )
     
---
+++ welcome-italanta-team/src/Welcom-All-Pages/Aljabana-Elizabeth/Aljabana.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import  imgAlja from "./images/lzy.jpg"
+
+function Aljabana() {
+  return (
+    <div>
+      
+      <img src={imgAlja} alt=""></img>
+
+        <h1>Vote of thanks to Microsoft team,Elewa and Windle Trust International.</h1>
+        <p>
+        I am writing to express my sincere gratitude to you for making the Italanta <br></br>program a successful journey. I was thrilled to learn programming skills<br></br> because my ambition was to become a software engineer, and I am <br></br>deeply appreciative of your support.
+        My heartfelt thanks to Jante and<br></br> Peter for the greatest sacrifice and support towards this academic <br></br>journey.
+        Am grateful to be among the beneficiaries of this program and I<br></br> believe that , the knowledge of programming I have gained from this program <br></br>is going to 
+        help me in solving technological problems arising from my society .
+
+
+
+        </p>
+        <h2>By Aljabana Eizabeth Mubarak</h2>
+    </div>
+  )
+}
+
+export default Aljabana
